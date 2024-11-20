e458602b006cf71dd4363a914fc6c7e3b36b822f
Yokohama Baystars <neouniverse2000masa@gmail.com>
2024-11-20 09:11:27 +0900

    DB作成時のコマンドにパスワードを追加

--- recipe/mysql/create_db.rb
+++ recipe/mysql/create_db.rb
@@ -1,9 +1,12 @@
+require 'dotenv'
+Dotenv.load
+
 mysql = node[:mysql]
 databases = node[:mysql][:databases]
 
 databases.each do |db|
     execute "mysql-create-database" do
-        command "sudo mysqladmin -u root create #{db}"
-        not_if "sudo mysqlshow -u root #{db}"
+        command "sudo mysqladmin -uroot -p#{ENV["DB_ROOT_PASSWORD"]} create #{db}"
+        not_if "sudo mysqlshow -uroot -p#{ENV["DB_ROOT_PASSWORD"]} #{db}"
     end
 end
