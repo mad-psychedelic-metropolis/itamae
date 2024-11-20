782513e75a0b718100607ee40707d68ee6acbe28
Yokohama Baystars <neouniverse2000masa@gmail.com>
2024-11-20 09:10:50 +0900

    初期パスワード出力先のファイルを変更

--- recipe/mysql/root_user.rb
+++ recipe/mysql/root_user.rb
@@ -6,22 +6,8 @@ mysqladmin_path = '/etc/my.cnf.d/mysql-admin.cnf'
 
 execute "set-root-password" do
     command <<-EOL
-       tmp_pass=$(grep 'temporary password' /var/log/mysql/mysqld.log | awk -F'root@localhost: ' '{print $2}') &&
+       tmp_pass=$(grep 'temporary password' /var/log/mysqld.log | awk -F'root@localhost: ' '{print $2}'  | tail -n 1) &&
        mysql -uroot -p"$tmp_pass" --connect-expired-password -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '#{ENV["DB_ROOT_PASSWORD"]}';"
     EOL
     not_if "mysql --defaults-file=#{mycnf_path} -uroot -e 'show databases;'"
-end
-
-template mysqladmin_path do
-    owner "mysql"
-    group "mysql"
-    mode "0600"
-    source "template#{mysqladmin_path}.erb"
-    variables(root_password: ENV["DB_ROOT_PASSWORD"])
-end
-
-file "/var/log/mysql/mysqld.log" do
-    owner "mysql"
-    group "mysql"
-    mode "0644"
 end
